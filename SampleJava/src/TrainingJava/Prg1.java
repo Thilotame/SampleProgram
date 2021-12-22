ed3cc8d1e2eccb5524017f950086b7ca5fcb9b13
2063250 <2063250@LTIN123202.cts.com>
2021-12-22 10:40:21 +0530

    First Project Uploaded

---
+++ SampleJava/src/TrainingJava/Prg1.java
@@ -0,0 +1,8 @@
+package TrainingJava;
+
+public class Prg1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("this is my first Java Program");
+	}
+}
